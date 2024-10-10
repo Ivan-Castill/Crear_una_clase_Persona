b691756673b0b35ad17d5b194c848eeeb18a71c9
Ivan-Castill <ivan2003castillo12345@gmail.com>
2024-10-09 22:30:33 -0500

    Tarea_S1_Ejercicio_01

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+
+class Persona {
+    private String nombre;
+    private int edad;
+    private String genero;
+
+    public Persona(String nombre, int edad, String genero) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.genero = genero;
+    }
+    public void presentarse() {
+        System.out.println("+++hello, soy " + nombre + " y mi edad es " + edad + " años, y mi genero es "+genero+".");
+    }
+}
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Persona persona1 = new Persona("Ana", 25, "Femenino");
+        Persona persona2 = new Persona("Luis", 30, "Masculino");
+        persona1.presentarse();
+        persona2.presentarse();
+        System.out.println("¡QUE TENGAN UN LINDO DIA/NOCHE!");
     }
 }
